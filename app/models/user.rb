b2b87e52838aa8b98de804fc86ae83725144d3cf
Amadou Ibrahim <amadou140589@gmail.com>
2020-01-31 17:42:59 +0100

    Add Associations and post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence:true, length: { minimum: 3}, uniqueness: { case_sensitive: false }
