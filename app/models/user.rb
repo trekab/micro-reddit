a8b4b2d0bbe17d1bb691f9f6acda77e833f514f0
Treasure Kabareebe <treasurecalm@gmail.com>
2020-01-31 19:04:52 +0300

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence:true, length: { minimum: 3}, uniqueness: { case_sensitive: false }
+  validates :email, presence:true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence:true
+
 end
